ea192357c9beedce2617e3a63d50e5f8555fd367
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-16 02:22:38 +0300

    route add

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
+    >
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>Hello World</h1>
+              <Link to='about'>About Us</Link>
+            </div>
+          }
+        />
+        <Route path='/about' element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
