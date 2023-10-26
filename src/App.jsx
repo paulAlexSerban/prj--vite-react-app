717a0a09641282de5194522599e824202e86838f
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-26 16:55:33 +0300

    feat: test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
                     path="/"
                     element={
                         <div>
-                            <h1>Hello World</h1>
+                            <h1>Hello World - TEST</h1>
                             <Link to="about">About Us</Link>
                         </div>
                     }
