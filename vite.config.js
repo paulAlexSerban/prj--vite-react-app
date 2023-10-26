cb584e53d3d9abfa31d33b47e2aa5f7cff158acc
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-26 13:55:12 +0300

    feat: chink split and vite configs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const { VITE_BASENAME } = process.env;
+import { dependencies } from './package.json';
+
+// https://sambitsahoo.com/blog/vite-code-splitting-that-works.html
+const renderChunks = (deps) =>{
+    let chunks = {};
+    Object.keys(deps).forEach((key) => {
+        chunks[key] = [key];
+    });
+    return chunks;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const { VITE_BASENAME } = env;
+
     const config = {
         plugins: [react()],
         base: VITE_BASENAME,
+        build: {
+            sourcemap: false ,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        ...renderChunks(dependencies),
+                    },
+                },
+            },
+        },
     };
 
     if (command !== 'serve') {
